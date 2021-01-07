85669dd65fa0241ceaf5ad9f51ec384d4d3038ea
Michael Woods <mrwoodzi@protonmail.com>
2021-01-07 08:41:22 -0600

    zedpy

---
+++ ex24.py
@@ -0,0 +1,12 @@
+print("Let's practice everything.")
+print('You\'d need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs.')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
