a8520faaff65481113fc83544d4ac88cc90f33d8
Michael Woods <mrwoodzi@protonmail.com>
2021-01-07 10:45:12 -0600

    zedpy

--- ex24.py
+++ ex24.py
@@ -1,7 +1,7 @@
-print("Let's practice everything.")
-print('You\'d need to know \'bout escapes with \\ that do:')
-print('\n newlines and \t tabs.')
-
+print("Let's practice everything.") #prints text
+print('You\'d need to know \'bout escapes with \\ that do:')#prints test with escap' ' \
+print('\n newlines and \t tabs.')#drops line and prints and has escape tabs
+#the program does not run the below program chronologically as it is just a variable
 poem = """
 \tThe lovely world
 with logic so firmly planted
@@ -9,28 +9,28 @@ cannot discern \n the needs of love
 nor comprehend passion from intuition
 and requires an explanation
 \n\t\twhere there is none.
-"""
+""" # triple quotes simply prints text invarable
 
-print("--------------")
-print(poem)
-print("--------------")
+print("--------------")#simple print
+print(poem)#is a variable that prints the poem
+print("--------------")#simple print
 
 
-five = 10 - 2 + 3 -6
-print(f"This should be five: {five}")
+five = 10 - 2 + 3 -6 #five is a varibale that works out a math problem
+print(f"This should be five: {five}") #prints text and then prints the variable
 
-def secret_formula(started):
+def secret_formula(started): # this is a mini script/command let that runs
     jelly_beans = started * 500
     jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
 
 
-start_point = 1000
+start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
-#remember that this is another way to format a cooked
-print("With a starting point of: {}".format(start_point))
+#remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point)) # line prints and then put 1000 into variable
 # it's just like with an f"" string
 print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
