d53220d67f099d785554ef813bee116d8839d962
Michael Woods <mrwoodzi@protonmail.com>
2021-01-09 09:20:30 -0600

    zedpy

--- ex24_test.py
+++ ex24_test.py
@@ -1,7 +1,15 @@
-print("Let's practice everything.") #prints text
-print('You\'d need to know \'bout escapes with \\ that do:')#prints test with escap' ' \
-print('\n newlines and \t tabs.')#drops line and prints and has escape tabs
-#the program does not run the below program chronologically as it is just a variable
+#I don't know what started is in line 2???
+def secret_formula(started): # this is a mini script/command let that runs
+    jelly_beans = started * 500
+    glass_jars = jelly_beans / 1000
+    crates = glass_jars / 100
+    return jelly_beans, glass_jars, crates
+
+five = 10 - 2 + 3 -6 #five is a varibale that works out a math problem
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+start_point = start_point / 10 #This is a variable that takes the start_point and divides it by 10
+formula = secret_formula(start_point)
 poem = """
 \tThe lovely world
 with logic so firmly planted
@@ -11,26 +19,16 @@ and requires an explanation
 \n\t\twhere there is none.
 """ # triple quotes simply prints text invarable
 
+print("Let's practice everything.") #prints text
+print('You\'d need to know \'bout escapes with \\ that do:')#prints test with escap' ' \
+print('\n newlines and \t tabs.')#drops line and prints and has escape tabs
+#the program does not run the below program chronologically as it is just a variable
+
 print("--------------")#simple print
 print(poem)#is a variable that prints the poem
 print("--------------")#simple print
 
-
-five = 10 - 2 + 3 -6 #five is a varibale that works out a math problem
 print(f"This should be five: {five}") #prints text and then prints the variable
-
-def secret_formula(started): # this is a mini script/command let that runs
-    jelly_beans = started * 500
-    jars = jelly_beans / 1000
-    crates = jars / 100
-    return jelly_beans, jars, crates
-
-
-start_point = 10000
-beans, jars, crates = secret_formula(start_point)
-start_point = start_point / 10 #This is a variable that takes the start_point and divides it by 10
-formula = secret_formula(start_point)
-
 #remember that this is another way to format a string
 print("With a starting point of: {}".format(start_point)) # line prints and then put 1000 into variable
 # it's just like with an f"" string
