b3fcb39c1e8db09caf362331abc7c454f8ab0688
Michael Woods <mrwoodzi@protonmail.com>
2021-01-12 15:01:58 -0600

    zedpy

--- ex25.py
+++ ex25.py
@@ -1,20 +1,20 @@
-def break_words(stuff):
+def break_words(stuff): # this breaks words into a [ , , , ] list
     """This function will break up words for us."""
     words = stuff.split(' ')
     return words
 
-def sort_words(words):
+def sort_words(words):# this puts words into a list [ , , , ,] in alphabetical order
     """Sorts the words."""
     return sorted(words)
 
-def print_first_words(words):
+def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
     print(word)
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1)
+    word = words.pop(-1)#Pop pops you to end/last word, I wonder if you can push to the beginning
     print(word)
 
 def sort_sentence(sentence):
