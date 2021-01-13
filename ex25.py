7826b0ee3300153b44bf7a7098b3ce2e34304088
Michael Woods <mrwoodzi@protonmail.com>
2021-01-13 10:44:14 -0600

    zedpy

--- ex25.py
+++ ex25.py
@@ -8,25 +8,25 @@ def sort_words(words):# this puts words into a list [ , , , ,] in alphabetical o
     return sorted(words)
 
 def print_first_word(words):
-    """Prints the first word after popping it off."""
+    """Prints the first word after popping off"""
     word = words.pop(0)
-    print(word)
+    print(word) 
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
     word = words.pop(-1)#Pop pops you to end/last word, I wonder if you can push to the beginning
-    print(word)
+    print(word)#this prints the popped word
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
-    words = break_words(sentence)
-    return sort_words(words)
+    words = break_words(sentence) #this break the words into a list [ , , ]
+    return sort_words(words)#this sorts broken words into alphabetical order
 
 def print_first_and_last(sentence):
     """Print the first and last words of the sentence"""
-    words = break_words(sentence)
-    print_first_word(words)
-    print_last_word(words)
+    words = break_words(sentence)#this break the words into a list [ , , ]
+    print_first_word(words)#prints first word in list
+    print_last_word(words)#prints last word in list
 
 def print_first_and_last_sorted(sentence):
     """Sorts the words then prints the first and last one."""
