3bc9bfe47dd39fced1df5b43948b885e5ac77a96
Michael Woods <mrwoodzi@protonmail.com>
2021-01-25 11:54:51 -0600

    zedpy

---
+++ ex30.py
@@ -0,0 +1,23 @@
+people = 30
+cars = 40
+trucks = 15
+
+#Is testing wether something is true and if so prints that
+if cars > people: #asks if this is true, in this case yes so prints "We should take the cars."
+    print("We should take the cars.")
+elif cars < people: 
+    print("We should not take the cars.")
+else: #if neither of the above are true print this
+    print("We can't decide.")
+#repeats the above process 
+if trucks > cars:
+    print("That's too many trucks.")
+elif trucks < cars:
+    print("Maybe we could take the trucks.")
+else:
+    print("We still can't decide.")
+
+if people > trucks:
+    print("Alright, let's just take the trucks.")
+else:
+    print("Fine, let's stay home then.")
