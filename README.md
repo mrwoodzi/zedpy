94642db3d45b02af39d6a004d4b0c410539b9ce7
Michael Woods <mrwoodzi@protonmail.com>
2021-01-30 10:55:46 -0600

    zedpy

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ I started this book in mid-Dec, 2020 and am presently working through it.
 Anything titled ex().py should be the original example that is given in the book. Anything with ex()_().py are modifications to the original files. 
 Shaw tells users of his book to modify/experiment with the files in various ways to see if they will run or pull an error.
 
+Please excuse the sophmoric humor and 80's throw back trivia that is found in some of my code.
+
 1/30/2021: I have really enjoyed Zed's book and as of this date am on ex32.py. I am officially working out of 3 main resources, 2 books and 1 internet source, freecodecamp.org.
     freecodecamp.org has been one of the best resources that I have found to date and has been a great companion to all the exercises I have been doing up to this point.
 
--- ex32_mycode.py
+++ ex32_mycode.py
@@ -5,7 +5,7 @@ hertz_change = [1.0, 1.0, 1.0]
 print('\n')
 
 for frequencies in brown_note:
-    print("\n") # for in cycles through every time so it adds a return to every line
+   # print("\n")  for in cycles through every time so it adds a return to every line
     print(f'\tThis is a brown note frequency {frequencies}.')
 
 print('\n')
@@ -15,11 +15,22 @@ for instruments in practice:
 
 
 # Let's try to build a list
-
+print("\n")
 tubas = []
 
 for increase in range(0, 3):
     print(f'\tYou can add more potential brown notes frequencies by adding {increase} hertz per frequency.')
 
-more_hertz = list(map(add, hertz_change, brownnote_hertz)) 
+print("\n")
+
+# I am trying to add 2 lists together, Zed doesn't go over this yet so I did an interweb search
+# I'm getting an error on add, don't know what to do 
+
+# more_hertz = list(map(add, hertz_change, brownnote_hertz)) 
+# my py didn't like add so let's try something else
+
+more_hertz = [sum(i) for i in zip(brownnote_hertz, hertz_change)]  
+# It works this way, maybe because it's all kept in a list
+for more in more_hertz:
+    print(f"\t{more} is another brown note frequency that can be hit.")
 
