d808033027ba85679c7982bec0fcd477b7eeed0a
Michael Woods <mrwoodzi@protonmail.com>
2021-02-03 12:44:04 -0600

    zedpy

--- ex35.py
+++ ex35.py
@@ -24,20 +24,21 @@ def bear_room():
     bear_moved = False
 
     while True:
-        choice = input("> ")
-
-        if choice == "take honey":
+        choice = input("> ") # I have no idea how to know what the options are to remove bear, needs a help
+        if choice == "help":
+            print("""You can "take honey" or "taunt bear".""")
+        if choice == "take honey": # this has to be worded "take honey" or it won't work
             dead("The bear looks at you then slaps your face off.")
-        elif choice == "taunt bear" and not bear_moved:
+        elif choice == "taunt bear" and not bear_moved:# has to be "taunt bear"
             print("The bear has moved from the door.")
-            print("You can go through it now.")
+            print("You can open the door now.") # I changed the wording of this so the user guided to what to input- "open door"
             bear_moved = True
         elif choice == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
         elif choice == "open door" and bear_moved:
             gold_room()
         else:
-            print("I got no idea what that means.")
+            print("What do you want to do?")
 
 
 def cthulu_room():
@@ -47,9 +48,9 @@ def cthulu_room():
 
     choice = input("> ")
 
-    if "flee" in choice:
+    if "flee" in choice: #as long as "flee" is in the input it head work
         start()
-    elif "head" in choice:
+    elif "head" in choice: # as long as "head" is in the input it head work
         dead("Well that was tasty!")
     else:
         cthulu_room()
